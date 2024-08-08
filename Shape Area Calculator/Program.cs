6c951970610aaf30424689ee3b7b7aeb088cadda
Xuho <you@example.com>
2024-08-08 11:35:49 +0700

    xuho push

---
+++ Shape Area Calculator/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+
+namespace Shape_Area_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            float result;
+
+            while (true)
+            {
+                Console.WriteLine("What shape would you like to find the area of?");
+                Console.WriteLine("1. Rectangle");
+                Console.WriteLine("2. Square");
+                Console.WriteLine("3. Triangle");
+                Console.WriteLine("4. Circle");
+                Console.WriteLine("5. Parallelogram");
+                Console.WriteLine("6. Trapezoid");
+                Console.WriteLine("7. Ellipse");
+                Console.WriteLine("8. Rhombus");
+                Console.WriteLine("9. Sector of a circle");
+                Console.WriteLine("10. Annulus");
+                Console.WriteLine("11. Kite");
+                Console.WriteLine("12. Exit");
+                Console.Write("Please select one: ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 12)
+                {
+                    Console.WriteLine("Thanks for using our app!");
+                    break;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter length:");
+                        float length = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter width:");
+                        float width = float.Parse(Console.ReadLine());
+
+                        result = length * width;
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter side:");
+                        float side = float.Parse(Console.ReadLine());
+
+                        result = (float)Math.Pow(side, 2);
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter base:");
+                        float baseTriangle = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter height:");
+                        float heightTriangle = float.Parse(Console.ReadLine());
+
+                        result = 1/2*baseTriangle*heightTriangle;
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter radius:");
+                        float radius = float.Parse(Console.ReadLine());
+
+                        result =(float)(Math.PI*Math.Pow(radius, 2));
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter base:");
+                        float basePara = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter height:");
+                        float heightPara = float.Parse(Console.ReadLine());
+
+                        result = basePara*heightPara;
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 6:
+                        Console.WriteLine("Enter base1:");
+                        float base1 = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter base2:");
+                        float base2 = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter height:");
+                        float heighttrap = float.Parse(Console.ReadLine());
+
+                        result = 1/2*(base1+base2)*heighttrap;
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 7:
+                        Console.WriteLine("Enter semi-major axis:");
+                        float semi_major = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter semi-minor axis:");
+                        float semi_minor = float.Parse(Console.ReadLine());
+
+                        result = (float)(Math.PI*semi_major*semi_minor);
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 8:
+                        Console.WriteLine("Enter diagonal1:");
+                        float dia1 = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter diagonal2:");
+                        float dia2 = float.Parse(Console.ReadLine());
+
+                        result = 1/2*dia1*dia2;
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 9:
+                        Console.WriteLine("Enter radius:");
+                        float radiusSector = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter angle in radians:");
+                        float angle = float.Parse(Console.ReadLine());
+
+                        result = (float)(1/2*Math.Pow(radiusSector, 2)*angle);
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 10:
+                        Console.WriteLine("Enter outer radius:");
+                        float outer = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter inner radius:");
+                        float inner = float.Parse(Console.ReadLine());
+
+                        result = (float)(Math.PI*Math.Pow(outer, 2)+Math.Pow(inner,2));
+                        Console.WriteLine("Result: " + result);
+                        Console.WriteLine();
+                        break;
+                    case 11:
+                        Console.WriteLine("Enter diagonal1:");
+                        float diago1 = float.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter diagonal2:");
+                        float diago2 = float.Parse(Console.ReadLine());
+
+                        result = 1/2*diago1*diago2;
+                        Console.WriteLine("Result: " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice, please select a valid option.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Shape Area Calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Shape Area Calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Shape Area Calculator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69c73cff-9378-44ed-a513-2ce2d05a9b31")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
